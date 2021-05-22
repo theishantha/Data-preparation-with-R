c010f4420ebe512a127aba635ed54187b7559d22
theishantha <ishanthaudara96@gmail.com>
2021-05-22 20:52:46 +0530

    using mean values

--- Data preparation.R
+++ Data preparation.R
@@ -2,4 +2,9 @@
 dataset = read.csv('Data.csv')
 
 #filling missing data
-
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
